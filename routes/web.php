ece5e7eb00032678af82d6317dcf1ec293872e14
Hana Hanifatul Hasanah <222212635@stis.ac.id>
2025-09-02 08:11:50 +0700

    make viewer

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('/viewer/home');
+});
+
+Route::get('/ketuatim', function () {
+    return view('/ketuatim/homeketua');
 });
 
 Route::get('/laporan/lap', function () {
