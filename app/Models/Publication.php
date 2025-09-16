9a05228359af2d8ee609798455439f877f969945
pii <snarsaggaf@gmail.com>
2025-09-16 16:01:48 +0700

    ga jelas ini hasil ai

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Publication extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'publications';
+    protected $primaryKey = 'publication_id';
+    
+    protected $fillable = [
+        'publication_name',
+        'user_id'
+    ];
+
+    // Relasi: Publication dimiliki oleh satu User
+    // public function user()
+    // {
+    //     return $this->belongsTo(User::class, 'user_id', 'user_id');
+    // }
+
+    // Relasi: Publication memiliki banyak Steps Plans
+    public function stepsPlans()
+    {
+        return $this->hasMany(StepsPlan::class, 'fk_publication_id', 'publication_id');
+    }
 }
