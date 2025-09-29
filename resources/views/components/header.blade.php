f3a8f3f1957e5138f18afbb80a08869f4b2c9d07
Hana Hanifatul Hasanah <222212635@stis.ac.id>
2025-09-29 20:41:59 +0700

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
 <div>
     <header class="relative bg-white shadow-sm">
         <div class="text-center py-5">
-            <h1 class="text-2xl font-bold">Dashboard Administrasi</h1>
-            <p class="text-sm text-gray-600">Sistem Manajemen Survei</p>
-            <p class="text-sm text-gray-600">Badan Pusat Statistik</p>
+            <h1 class="text-2xl font-bold">Dashboard SIMKA</h1>
+            <p class="text-sm text-gray-600">Sistem Monitoring Kegiatan</p>
+            <p class="text-sm text-gray-600">Badan Pusat Statistik Kota Bekasi</p>
         </div>
     </header>
 </div>
