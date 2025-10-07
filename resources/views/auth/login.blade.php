bb7362ee76e34ffb85771a9b5f0540a0a4f1e61a
pii <snarsaggaf@gmail.com>
2025-10-07 18:18:06 +0700

    benerin login dikit doang

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,31 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <!-- Tailwind Elements -->
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/alpinejs" defer></script>
+    <script src="/js/admin.js" defer></script>
 
-@section('content')
+</head>
+
+<body>
+    <body>
+    <div>
+        {{-- Navbar --}}
+        <x-navbar ></x-navbar>
+        {{-- Header --}}
+        <x-header></x-header>
+    </div>
+
+    <main>
  <div class="flex flex-col h-screen md:flex-row">
         <!-- Kiri: Deskripsi -->
         <div
@@ -73,4 +98,6 @@
             </form>
         </div>
     </div>
-@endsection
+</main>
+</body>
+</html>
